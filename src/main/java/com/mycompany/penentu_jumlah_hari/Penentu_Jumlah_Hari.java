88aae945137ab0876524769f4cb0aa7bbaf019c5
syarif027 <syarifhidayat2791@gmail.com>
2023-11-18 18:05:43 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/penentu_jumlah_hari/Penentu_Jumlah_Hari.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.penentu_jumlah_hari;
+
+/**
+ *
+ * @author User
+ */
+public class Penentu_Jumlah_Hari {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
